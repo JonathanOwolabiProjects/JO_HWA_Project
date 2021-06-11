54850ec837708674fdb38283664721a8d39705dd
JOwolabi <jowolabi@qa.com>
2021-06-11 17:41:02 +0100

    JOHWA-7 imporovements made to the house related classes

--- Back_end/HWA_Project_Backend/src/main/java/com/qa/HWAProject/domain/Portfolio.java
+++ Back_end/HWA_Project_Backend/src/main/java/com/qa/HWAProject/domain/Portfolio.java
@@ -1,52 +1,52 @@
-package com.qa.HWAProject.domain;
-
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-@Entity
-public class Portfolio {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-
-	private String name;
-	
-	@OneToMany(mappedBy = "portfolio")
-	@JsonIgnore
-	private List<House> houses;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<House> getHouses() {
-		return houses;
-	}
-
-	public void setHouses(List<House> houses) {
-		this.houses = houses;
-	}
-	
-	
-
-}
+//package com.qa.HWAProject.domain;
+//
+//import java.util.List;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//
+//@Entity
+//public class Portfolio {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer id;
+//
+//	private String name;
+//	
+//	@OneToMany(mappedBy = "portfolio")
+//	@JsonIgnore
+//	private List<House> houses;
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public List<House> getHouses() {
+//		return houses;
+//	}
+//
+//	public void setHouses(List<House> houses) {
+//		this.houses = houses;
+//	}
+//	
+//	
+//
+//}
