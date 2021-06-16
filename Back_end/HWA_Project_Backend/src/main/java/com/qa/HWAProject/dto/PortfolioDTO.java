76be0ec08e8dd6d15a9caf8cd61e228fb6bf511c
JOwolabi <jowolabi@qa.com>
2021-06-16 09:49:09 +0100

    JOHWA-3 - creation of dto files and improvement to files

--- Back_end/HWA_Project_Backend/src/main/java/com/qa/HWAProject/dto/PortfolioDTO.java
+++ Back_end/HWA_Project_Backend/src/main/java/com/qa/HWAProject/dto/PortfolioDTO.java
@@ -1,27 +1,18 @@
-package com.qa.HWAProject.domain;
+package com.qa.HWAProject.dto;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import com.qa.HWAProject.domain.House;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-@Entity
-public class Portfolio {
+public class PortfolioDTO {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String name;
 	
-	@OneToMany(mappedBy = "portfolio")
-	@JsonIgnore
 	private List<House> houses;
+	
+	// GETTERS AND SETTERS 
 
 	public Integer getId() {
 		return id;
@@ -46,7 +37,6 @@ public class Portfolio {
 	public void setHouses(List<House> houses) {
 		this.houses = houses;
 	}
-	
 	
 
 }
